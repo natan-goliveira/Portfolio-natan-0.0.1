708b5e35daa4c15582b86576cc6ab4ffd28dd0df
Natan Gomes <natantatan2000@gmail.com>
2025-07-22 01:02:07 -0300

    feat: correção do menu/teste de animação hero

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
-import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const mobileMenuButton = document.getElementById("mobile-menu-button");
